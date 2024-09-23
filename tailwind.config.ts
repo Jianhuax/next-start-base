78b5013cacbc33509fbd29f551c9337115cce752
Jianhuax <jianhua@jianhua.me>
2024-09-23 15:13:05 -0400

    init commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,34 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
+		screens: {
+			xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+		},
   },
-  plugins: [],
+  plugins: [
+		plugin(function ({ addBase, addComponents }) {
+      addBase({});
+      addComponents({
+        ".container": {
+          "@apply max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-16 xl:max-w-[87.5rem]":
+            {},
+        },
+			});
+		}),
+	],
 };
 export default config;
